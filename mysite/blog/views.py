54df952ca60d326cf37da84ff4ffc455d9fdae85
kirmolaa <kirmolaa@gmail.com>
2024-05-11 15:46:42 +0000

    created templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from blog.models import Post, Category, Tag
 
-# Create your views here.
+
+
+class PostView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
+    
